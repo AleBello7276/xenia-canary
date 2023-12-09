1091dec9fb0e72ac098bef2d8b49fecced14665c
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2023-12-09 22:15:39 +0000

    [XAM] Implemented XNetLogonGetMachineID

--- src/xenia/app/emulator_window.cc
+++ src/xenia/app/emulator_window.cc
@@ -1916,7 +1916,7 @@ void EmulatorWindow::NetplayStatus() {
          xe::string_util::BoolToString(xe::kernel::XLiveAPI::is_active());
   msg += "\n";
 
-  if (xe::kernel::XLiveAPI::is_intsalised() && cvars::upnp) {
+  if (xe::kernel::XLiveAPI::is_initialized() && cvars::upnp) {
     if (xe::kernel::XLiveAPI::upnp_handler.is_active()) {
       msg += "UPnP: Device found";
     } else {
@@ -1932,7 +1932,7 @@ void EmulatorWindow::NetplayStatus() {
   msg += "Offline Mode: " + xe::string_util::BoolToString(cvars::offline_mode);
   msg += "\n";
 
-  if (xe::kernel::XLiveAPI::is_intsalised()) {
+  if (xe::kernel::XLiveAPI::is_initialized()) {
     msg += "\n";
 
     if (xe::kernel::XLiveAPI::is_active()) {
